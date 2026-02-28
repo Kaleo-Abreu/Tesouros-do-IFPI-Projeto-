dd1f1239970394a3e80e0d20628439c8f88f16ba
willianjonhy05 <willianjohny05@gmail.com>
2026-02-28 20:15:34 -0300

    Página de votação... editando

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Função para finalizar a votação e redirecionar para a página de confirmação
+function finalizarVotacao() {
+    // Exemplo de mensagem de confirmação (você pode personalizar a mensagem conforme necessário)
+    alert("Votação confirmada com sucesso!");
+
+    // Redireciona para a página de confirmação
+    window.location.href = "confirmacao.html"; // Aqui a página confirmacao.html será carregada
+}
